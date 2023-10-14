1af1b62bf783b802ae1e1ab0ea9c217ea50341f7
nigaraliyevas <naliyeva965@gmail.com>
2023-10-15 00:58:29 +0400

    feat:tasks completed

---
+++ Array-Objects/Array-Objects/Array-Objects/Program.cs
@@ -0,0 +1,86 @@
+﻿#region task-1
+//int[] numbers = {5,7,9,6,7};
+//int sum = 0;
+//for(int i = 0; i < numbers.Length; i++)
+//{
+//    sum +=numbers[i];
+//}
+//Console.WriteLine(sum);
+#endregion
+#region task-2
+//int[] numbers = {5,17,15,6,25,4};
+//int count = 0;
+//for(int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > 10)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+#endregion
+#region task-3
+//int[] numbers = { 5, 1, 4, 6, 8, 4 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    sum += numbers[i];
+//}
+
+//Console.WriteLine(sum / numbers.Length);
+#endregion
+#region task-4
+string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+foreach (string month in months)
+{
+    if (month == "January" || month == "February" || month == "December")
+    {
+        Console.WriteLine(month + " Winter season");
+    }
+}
+
+#endregion
+#region task-1
+//int[] numbers = {5,7,9,6,7};
+//int sum = 0;
+//for(int i = 0; i < numbers.Length; i++)
+//{
+//    sum +=numbers[i];
+//}
+//Console.WriteLine(sum);
+#endregion
+#region task-2
+//int[] numbers = {5,17,15,6,25,4};
+//int count = 0;
+//for(int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > 10)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+#endregion
+#region task-3
+//int[] numbers = { 5, 1, 4, 6, 8, 4 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    sum += numbers[i];
+//}
+
+//Console.WriteLine(sum / numbers.Length);
+#endregion
+#region task-4
+string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+foreach (string month in months)
+{
+    if (month == "January" || month == "February" || month == "December")
+    {
+        Console.WriteLine(month + " Winter season");
+    }
+}
+
+#endregion
