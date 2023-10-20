104b8acdb37be5e8658be68181459ca02a9ba6e2
nigaraliyevas <naliyeva965@gmail.com>
2023-10-20 18:50:02 +0400

    feat:New task is done before deadline

---
+++ BigO/BioO.cs
@@ -0,0 +1,72 @@
+﻿#region Task-1
+#region solution-1
+/*int[] array = { 15, 123 };  //daxiline bir neche eded yazib toplamaq mumkundur👌
+int sum = 0;
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    sum += array[i];
+}
+while (sum > 0)
+{
+    sum /= 10;
+    count++;
+}
+if (count == 2)
+{
+    Console.WriteLine("Sum is 2 digit number");
+}
+else if (count == 3)
+{
+    Console.WriteLine("Sum is 3 digit number");
+}
+else
+{
+    Console.WriteLine("Not considered");
+}*/
+#endregion
+#region solution-2
+/*int a = 99;
+int b = 1;
+int sum = a + b;
+int count = 0;
+while (sum > 0)
+{
+    sum /= 10;
+    count++;
+}
+if (count == 2)
+{
+    Console.WriteLine("Sum is 2 digit number");
+}
+else if(count == 3)
+{
+    Console.WriteLine("Sum is 3 digit number");
+}
+else
+{
+    Console.WriteLine("Not considered");
+*/
+#endregion
+#endregion
+#region Task-2
+/*int heightOfCabinet=4;
+int widthOfCabinet = 5;
+int lenghthOfCabinet = 6;
+int heightOfDoor = 10;
+int widthOfDoor = 20;
+if ((heightOfDoor >= heightOfCabinet && widthOfDoor >= widthOfCabinet) ||
+        (heightOfDoor >= widthOfCabinet && widthOfDoor >= heightOfCabinet) ||
+        (heightOfDoor >= widthOfCabinet && widthOfDoor >= lenghthOfCabinet) ||
+        (heightOfDoor >= lenghthOfCabinet && widthOfDoor >= widthOfCabinet) ||
+        (heightOfDoor >= heightOfCabinet && widthOfDoor >= lenghthOfCabinet) ||
+        (heightOfDoor >= lenghthOfCabinet && widthOfCabinet >= heightOfCabinet)
+   )
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}*/
+#endregion
