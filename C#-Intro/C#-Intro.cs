074cc7326358adfb23823e7a78f644d3068f10bf
nigaraliyevas <naliyeva965@gmail.com>
2023-10-23 16:28:37 +0400

    feat:GW64 ~/Desktop/Code Academy/Tasks/Task-4_09.10.23 (main)

---
+++ C#-Intro/C#-Intro.cs
@@ -0,0 +1,63 @@
+﻿#region task-1
+// 4 reqemli eded verilib. Bu ededin eveline 7 reqemini , axirinada 8 reqemini artir. Example1: 333= 73338
+/*Console.WriteLine("Please enter a 4 digit number:");
+int num = int.Parse(Console.ReadLine());
+string result = $"7{num}8";
+Console.WriteLine(result);*/
+#endregion
+#region task-2
+//3 reqemli eded verilib. Bu ededin axirina hemin ededdin ozunu yapishdir. example: 333= 333333;
+/*Console.WriteLine("Please enter a 3 digit number:");
+int num = int.Parse(Console.ReadLine());
+string result = $"{num}{num}";
+Console.WriteLine(result);*/
+#endregion
+#region task-3
+//5 reqemli eded verilib. Bu ededin evvel 18 % sonra ise 3 % tap.
+/*Console.WriteLine("Please enter a 5 digit number");
+int num = int.Parse(Console.ReadLine());
+double resultPercentOf18 = (num * 0.18);
+double resultPercentOf3 = (num * 0.03);
+Console.WriteLine($"The 18 percent of {num} is {resultPercentOf18} and 3 percent of {num} is {resultPercentOf3}");*/
+#endregion
+#region task-4
+//3 reqemli eded verilib. Bu ededin axirina 7 artir. Sonra cavabin 7% tap;
+/*Console.WriteLine("Please enter a 3 digit number:");
+int num=int.Parse(Console.ReadLine());
+string added7 = num+"7";
+int turnedNum = int.Parse(added7);
+double result = turnedNum * 0.07;
+Console.WriteLine($"The entered number is {num}. Then added 7 end of the number like this:{turnedNum},then found 7 percent of {turnedNum}, result is {result}");*/
+#endregion
+#region task-5
+//4 reqemli eded verilib. Bu ededin evvelin 4 reqemi , axirina ise 44 ededini artir. Alinan cavabin 44 % tap.
+/*Console.WriteLine("Please enter a 4 digit number:");
+int num=int.Parse(Console.ReadLine());
+string added4And44 = "4"+num+"44";
+int turnedNum = int.Parse(added4And44);
+double result = turnedNum * 0.44;
+Console.WriteLine($"The entered number is {num}. Then added 4 in front of number and 44 end of the number like this:{turnedNum},then found 44 percent of {turnedNum}, result is {result}");*/
+#endregion
+#region task-6
+//4 reqemli eded verilib. Bu ededin evvel 20%-ni , sonra ise cavabin 10% tap. Alinan cavabin kvadratini tap.
+/*Console.WriteLine("Please enter a 4 digit number:");
+int num=int.Parse(Console.ReadLine());
+double percent20 = num * .2;
+double percent10 = percent20 * .1;
+double result = percent10 * percent10;
+Console.WriteLine($"The entered number is {num}. 20 percent of {num} is {percent20}. 10 percent of {percent20} is {percent10}.The final result is {result}.");*/
+#endregion
+#region task-7
+//2 dene 5 reqemli eded verilib. Bu ededleri toplayib. Alinan cavabin evveline ve axirina 5 artir. Neticenin 5 % tap
+/*Console.WriteLine("Please enter a first 5 digit number:");
+int firstNum=int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter a second 5 digit number:");
+int secondNum=int.Parse(Console.ReadLine());
+int sum=firstNum+secondNum;
+string added5="5"+sum+"5";
+int turnedInteger=int.Parse(added5);
+double result = turnedInteger * .05;
+Console.WriteLine($"Sum is {firstNum + secondNum}.Added 5 in front and end of number {turnedInteger}.5 percent of {turnedInteger} is {result}.");*/
+
+#endregion
+//done👌
