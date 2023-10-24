92b1cdf43b1dfa61b5d3bade4856e4c6bb3942a3
nigaraliyevas <naliyeva965@gmail.com>
2023-10-24 23:41:19 +0400

    refactor:Changed task-2 as asked

--- Algorithm-Loop/calculating-program/calculating-program/Program.cs
+++ Algorithm-Loop/calculating-program/calculating-program/Program.cs
@@ -15,16 +15,16 @@ Console.WriteLine(count);
 #endregion
 #region task-2
 //Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapın.
-/*int n = 44;
-if (n == 1)
+int n = 44;
+int count = 0;
+for (int i = 1; i <= n; i++)
 {
-    Console.WriteLine($"{n} hem sade hemde murekkeb ededdir");
+    if(n % i == 0)
+    {
+        count++;
+    }
 }
-else if( n==2 || n == 3)
-{
-    Console.WriteLine($"{n} sade ededdir");
-}
-else if(n%2==0 || n % 3 == 0)
+if (count >2)
 {
     Console.WriteLine($"{n} murekkeb ededdir");
 }
@@ -32,7 +32,7 @@ else
 {
     Console.WriteLine($"{n} sade ededdir");
 }
-*/
+
 #endregion
 #region task-3
 //Verilmiş müsbət tam n ədədinin 2-nin qüvvəti olub-olmamasını tapın
@@ -63,7 +63,7 @@ for (int i=0; i<n;i++)
 }*/
 #endregion
 #region solution-2
-int n = 0;
+/*int n = 0;
 int i = n;
 if (i > 0)
 {
@@ -79,7 +79,7 @@ if (i == 1)
 else
 {
     Console.WriteLine($"{n} ededi 2-nin quvveti deyil.");
-}
+}*/
 
 #endregion
 #endregion
